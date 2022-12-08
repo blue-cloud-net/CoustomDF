8681e6b5dbc019caa739e1e7601a60b7f9c5305f
huansky <651494458@qq.com>
2022-12-08 18:45:34 +0800

    feat:添加字符串扩展

---
+++ framework/FantasySky.CustomDF.Core/System/StringExtensions.cs
@@ -0,0 +1,71 @@
+namespace System;
+
+/// <summary>
+/// Extension methods for <see cref="String"/>.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Indicates whether this string is null or an System.String.Empty string.
+    /// </summary>
+    public static bool IsNullOrEmpty(this string str)
+    {
+        return String.IsNullOrEmpty(str);
+    }
+
+    /// <summary>
+    /// indicates whether this string is null, empty, or consists only of white-space characters.
+    /// </summary>
+    public static bool IsNullOrWhiteSpace(this string str)
+    {
+        return String.IsNullOrWhiteSpace(str);
+    }
+
+    /// <summary>
+    /// Converts line endings in the string to <see cref="Environment.NewLine"/>.
+    /// </summary>
+    public static string NormalizeLineEndings(this string str)
+    {
+        return str.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", Environment.NewLine);
+    }
+
+    /// <summary>
+    /// Remove all line endings.
+    /// </summary>
+    public static string RemoveLineEndings(this string str)
+    {
+        return str.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", String.Empty);
+    }
+
+    /// <summary>
+    /// Uses string.Split method to split given string by given separator.
+    /// </summary>
+    public static string[] Split(this string str, string separator)
+    {
+        return str.Split(new[] { separator }, StringSplitOptions.None);
+    }
+
+    /// <summary>
+    /// Uses string.Split method to split given string by given separator.
+    /// </summary>
+    public static string[] Split(this string str, string separator, StringSplitOptions options)
+    {
+        return str.Split(new[] { separator }, options);
+    }
+
+    /// <summary>
+    /// Uses string.Split method to split given string by <see cref="Environment.NewLine"/>.
+    /// </summary>
+    public static string[] SplitToLines(this string str)
+    {
+        return str.Split(Environment.NewLine);
+    }
+
+    /// <summary>
+    /// Uses string.Split method to split given string by <see cref="Environment.NewLine"/>.
+    /// </summary>
+    public static string[] SplitToLines(this string str, StringSplitOptions options)
+    {
+        return str.Split(Environment.NewLine, options);
+    }
+}
