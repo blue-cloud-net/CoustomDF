531002738742a76799d9cbedd07c51ede822fe15
huansky <651494458@qq.com>
2022-12-21 19:18:29 +0800

    feat:添加仓库接口

---
+++ framework/src/FantasySky.CustomDF.Ddd.Domain/FantasySky/CustomDF/Domain/Repositories/IReadOnlyRepository.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+using FantasySky.CustomDF.Domain.Entities;
+
+namespace FantasySky.CustomDF.Domain.Repositories;
+
+public interface IReadOnlyRepository<TEntity> : IReadOnlyBasicRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    //IAsyncQueryableExecuter AsyncExecuter { get; }
+
+    Task<IQueryable<TEntity>> WithDetailsAsync(CancellationToken cancellationToken);
+
+    Task<IQueryable<TEntity>> WithDetailsAsync(CancellationToken cancellationToken,params Expression<Func<TEntity, object>>[] propertySelectors);
+
+    Task<IQueryable<TEntity>> GetQueryableAsync(CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Gets a list of entities by the given <paramref name="predicate"/>.
+    /// </summary>
+    /// <param name="predicate">A condition to filter the entities</param>
+    /// <param name="includeDetails">Set true to include details (sub-collections) of this entity</param>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
+    Task<List<TEntity>> GetListAsync(
+        [NotNull] Expression<Func<TEntity, bool>> predicate,
+        bool includeDetails = false,
+        CancellationToken cancellationToken = default);
+}
+
+public interface IReadOnlyRepository<TEntity, TKey> : IReadOnlyRepository<TEntity>, IReadOnlyBasicRepository<TEntity, TKey>
+    where TEntity : class, IEntity<TKey>
+{
+
+}
