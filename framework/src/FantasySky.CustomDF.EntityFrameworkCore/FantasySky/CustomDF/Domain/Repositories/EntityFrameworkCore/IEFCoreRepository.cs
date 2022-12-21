665552c1299514b83805e5549e77d7f5f5ea5e2f
huansky <651494458@qq.com>
2022-12-21 19:18:46 +0800

    feat:添加EFCore仓库接口

---
+++ framework/src/FantasySky.CustomDF.EntityFrameworkCore/FantasySky/CustomDF/Domain/Repositories/EntityFrameworkCore/IEFCoreRepository.cs
@@ -0,0 +1,15 @@
+namespace FantasySky.CustomDF.Domain.Repositories.EntityFrameworkCore;
+
+public interface IEfCoreRepository<TEntity> : IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    Task<DbContext> GetDbContextAsync();
+
+    Task<DbSet<TEntity>> GetDbSetAsync();
+}
+
+public interface IEfCoreRepository<TEntity, TKey> : IEfCoreRepository<TEntity>, IRepository<TEntity, TKey>
+    where TEntity : class, IEntity<TKey>
+{
+
+}
