38e76ecfe3f7ff39ea9513cd8e0f139d9bb6050d
sky <bluecloudnet@outlook.com>
2023-01-02 21:28:59 +0800

    feat:补充额外的实现

---
+++ framework/src/Caching/FantasySky.CustomDF.Caching/FantasySky/CustomDF/Caching/ICacheSupportsSortSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace FantasySky.CustomDF.Caching;
+
+public interface ICacheSupportsSortSet
+{
+    bool SortedSetAdd(string key, double order, byte[] value, DistributedCacheEntryOptions options);
+}
