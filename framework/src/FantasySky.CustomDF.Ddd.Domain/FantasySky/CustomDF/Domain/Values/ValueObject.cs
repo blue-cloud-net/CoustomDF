830d93c9a7b39770e80780efacb7e1a48a65e183
huansky <651494458@qq.com>
2023-02-06 17:16:57 +0800

    feat:添加ValueObject

---
+++ framework/src/FantasySky.CustomDF.Ddd.Domain/FantasySky/CustomDF/Domain/Values/ValueObject.cs
@@ -0,0 +1,46 @@
+namespace FantasySky.CustomDF.Ddd.Domain.Values;
+
+//Inspired from https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/microservice-ddd-cqrs-patterns/implement-value-objects
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetAtomicValues();
+
+    public bool ValueEquals(object obj)
+    {
+        if (obj == null || obj.GetType() != this.GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        var thisValues = this.GetAtomicValues().GetEnumerator();
+        var otherValues = other.GetAtomicValues().GetEnumerator();
+
+        var thisMoveNext = thisValues.MoveNext();
+        var otherMoveNext = otherValues.MoveNext();
+        while (thisMoveNext && otherMoveNext)
+        {
+            if (thisValues.Current is null ^ otherValues.Current is null)
+            {
+                return false;
+            }
+
+            if (thisValues.Current != null && !thisValues.Current.Equals(otherValues.Current))
+            {
+                return false;
+            }
+
+            thisMoveNext = thisValues.MoveNext();
+            otherMoveNext = otherValues.MoveNext();
+
+            if (thisMoveNext != otherMoveNext)
+            {
+                return false;
+            }
+        }
+
+        return !thisMoveNext && !otherMoveNext;
+    }
+}
