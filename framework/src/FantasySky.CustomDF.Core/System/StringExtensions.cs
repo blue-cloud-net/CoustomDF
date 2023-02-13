247fa8e945dbd84f337221afd9283b0ba4b2ba42
huansky <651494458@qq.com>
2023-02-13 17:52:02 +0800

    feat:基础扩展

--- framework/src/FantasySky.CustomDF.Core/System/StringExtensions.cs
+++ framework/src/FantasySky.CustomDF.Core/System/StringExtensions.cs
@@ -41,7 +41,7 @@ public static class StringExtensions
     /// <summary>
     /// indicates whether this string is null, empty, or consists only of white-space characters.
     /// </summary>
-    public static bool IsNullOrWhiteSpace([NotNullWhen(false)]this string? str)
+    public static bool IsNullOrWhiteSpace([NotNullWhen(false)] this string? str)
     {
         return String.IsNullOrWhiteSpace(str);
     }
@@ -60,6 +60,122 @@ public static class StringExtensions
     public static string RemoveLineEndings(this string str)
     {
         return str.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", String.Empty);
+    }
+
+    /// <summary>
+    /// Removes first occurrence of the given postfixes from end of the given string.
+    /// </summary>
+    /// <param name="str">The string.</param>
+    /// <param name="postFixes">one or more postfix.</param>
+    /// <returns>Modified string or the same string if it has not any of given postfixes</returns>
+    public static string RemovePostFix(this string str, params string[] postFixes)
+    {
+        return str.RemovePostFix(StringComparison.Ordinal, postFixes);
+    }
+
+    /// <summary>
+    /// Removes first occurrence of the given postfixes from end of the given string.
+    /// </summary>
+    /// <param name="str">The string.</param>
+    /// <param name="comparisonType">String comparison type</param>
+    /// <param name="postFixes">one or more postfix.</param>
+    /// <returns>Modified string or the same string if it has not any of given postfixes</returns>
+    public static string RemovePostFix(this string str, StringComparison comparisonType, params string[] postFixes)
+    {
+        if (str.IsNullOrEmpty())
+        {
+            return str;
+        }
+
+        if (postFixes.IsNullOrEmpty())
+        {
+            return str;
+        }
+
+        foreach (var postFix in postFixes)
+        {
+            if (str.EndsWith(postFix, comparisonType))
+            {
+                return str.Left(str.Length - postFix.Length);
+            }
+        }
+
+        return str;
+    }
+
+    /// <summary>
+    /// Removes first occurrence of the given prefixes from beginning of the given string.
+    /// </summary>
+    /// <param name="str">The string.</param>
+    /// <param name="preFixes">one or more prefix.</param>
+    /// <returns>Modified string or the same string if it has not any of given prefixes</returns>
+    public static string RemovePreFix(this string str, params string[] preFixes)
+    {
+        return str.RemovePreFix(StringComparison.Ordinal, preFixes);
+    }
+
+    /// <summary>
+    /// Removes first occurrence of the given prefixes from beginning of the given string.
+    /// </summary>
+    /// <param name="str">The string.</param>
+    /// <param name="comparisonType">String comparison type</param>
+    /// <param name="preFixes">one or more prefix.</param>
+    /// <returns>Modified string or the same string if it has not any of given prefixes</returns>
+    public static string RemovePreFix(this string str, StringComparison comparisonType, params string[] preFixes)
+    {
+        if (str.IsNullOrEmpty())
+        {
+            return str;
+        }
+
+        if (preFixes.IsNullOrEmpty())
+        {
+            return str;
+        }
+
+        foreach (var preFix in preFixes)
+        {
+            if (str.StartsWith(preFix, comparisonType))
+            {
+                return str.Right(str.Length - preFix.Length);
+            }
+        }
+
+        return str;
+    }
+
+    /// <summary>
+    /// Gets a substring of a string from beginning of the string.
+    /// </summary>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="str"/> is null</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="len"/> is bigger that string's length</exception>
+    public static string Left(this string str, int len)
+    {
+        Check.IsNotNull(str, nameof(str));
+
+        if (str.Length < len)
+        {
+            throw new ArgumentException("len argument can not be bigger than given string's length!");
+        }
+
+        return str[..len];
+    }
+
+    /// <summary>
+    /// Gets a substring of a string from end of the string.
+    /// </summary>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="str"/> is null</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="len"/> is bigger that string's length</exception>
+    public static string Right(this string str, int len)
+    {
+        Check.IsNotNull(str, nameof(str));
+
+        if (str.Length < len)
+        {
+            throw new ArgumentException("len argument can not be bigger than given string's length!");
+        }
+
+        return str[(str.Length - len)..len];
     }
 
     /// <summary>
